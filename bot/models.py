0c362db79d939f572ee0293d78ddf20d15108eb9
Dragneel343 <37350944+Dragneel343@users.noreply.github.com>
2020-12-03 13:08:24 -0700

    DB class changes

--- bot/models.py
+++ bot/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
 class userInfo(models.Model):
-  username = models.TextField()
-  password = models.TextField()
+  #personal info
+  first_name = models.TextField()
+  last_name = models.TextField()
+  email = models.EmailField()
+  #shipping info
+  addr = models.TextField()
+  city = models.TextField
+  state = models.TextField()
+  zip_code = models.TextField()
+  #card info
+  card_number = models.IntegerField()
+  exp_date = models.DateField()
+  ccv = models.IntegerField()
+  #product info
+  product_id = models.TextField()
+  shoe_size = models.FloatField()
