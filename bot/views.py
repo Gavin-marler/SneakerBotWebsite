73de3475bba95a9b668d3e88009f835d5fde1d2a
Tanner Robinson <Rob16041@byui.edu>
2020-12-04 14:03:39 -0700

    Finished Database

--- bot/views.py
+++ bot/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from bot.models import userInfo
 
 def getIndex(request):
   return render(request,'index.html')
@@ -8,4 +9,17 @@ def getForm(request):
 
 def runBot(request):
   print(request.GET)
+  user = userInfo(first_name = request.GET["first_name"],
+                  last_name = request.GET["last_name"],
+                  email = request.GET["email"],
+                  addr = request.GET["addr"],
+                  city = request.GET["city"],
+                  state = request.GET["state"],
+                  zip_code = request.GET["zip_code"],
+                  card_number = request.GET["card_number"],
+                  exp_date = request.GET["exp_date"],
+                  ccv = request.GET["ccv"],
+                  product_id = "newProduct",
+                  shoe_size = request.GET["shoe_size"])
+  user.save()
   return redirect("index")
